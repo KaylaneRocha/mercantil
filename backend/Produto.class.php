030037650437f954dbf5311d72ca00881b917ad2
Davi <davicavalcante.itap.ce@gmail.com>
2021-01-04 15:50:18 -0300

    UPDATE EM ARQUIVOS CONFLITADOS

--- backend/Produto.class.php
+++ backend/Produto.class.php
@@ -1,190 +1,153 @@
 <?php
 
-    require_once "Conexao.class.php";
+require_once "Conexao.class.php";
 
-    class Produto {
+class Produto
+{
 
-        // Cadastro
-<<<<<<< HEAD
-        public function cadastrar($titulo,$descricao,$quantidade,$preco) {
-=======
-        public function cadastrar($titulo, $descricao,$quantidade) {
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-            $conexao = new Conexao();
-            $conn = $conexao->conectar();
+    // Cadastro
+    public function cadastrar($titulo, $descricao, $quantidade, $preco)
+    {
 
-            try {
-<<<<<<< HEAD
-                $sql = "INSERT INTO produto VALUES (null,:titulo,:descricao,:quantidade,:preco);";
-=======
-                $sql = "INSERT INTO pessoas VALUES (null, :titulo,:descricao, :quantidade);";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
+        $conexao = new Conexao();
+        $conn = $conexao->conectar();
 
-                $consulta = $conn->prepare($sql);
+        try {
 
-                $consulta->bindValue(":titulo", $titulo);
-                $consulta->bindValue(":descricao", $descricao);
-                $consulta->bindValue(":quantidade", $quantidade);
-<<<<<<< HEAD
-                $consulta->bindValue(":preco", $preco);
-=======
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
+            $sql = "INSERT INTO produto VALUES (null,:titulo,:descricao,:quantidade,:preco);";
 
-                $retorno = $consulta->execute();
 
-                if($retorno) {
-<<<<<<< HEAD
-                    echo "Produto cadastrado";
-=======
-                    echo "Pessoa cadastrada";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-                } else {
-                    echo "Erro";
-                }
-            } catch(PDOException $e) {
-                echo $e;
+            $consulta = $conn->prepare($sql);
+
+            $consulta->bindValue(":titulo", $titulo);
+            $consulta->bindValue(":descricao", $descricao);
+            $consulta->bindValue(":quantidade", $quantidade);
+
+            $consulta->bindValue(":preco", $preco);
+
+
+            $retorno = $consulta->execute();
+
+            if ($retorno) {
+
+                echo "Produto cadastrado";
+            } else {
+                echo "Erro";
             }
+        } catch (PDOException $e) {
+            echo $e;
         }
-
-        // Listar
-<<<<<<< HEAD
-        public function listar() {
-=======
-     /*   public function listar() {
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-            $conexao = new Conexao();
-            $conn = $conexao->conectar();
-
-            try {
-<<<<<<< HEAD
-                $sql = "SELECT * FROM produto;";
-=======
-                $sql = "SELECT * FROM pessoas;";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-
-                $consulta = $conn->prepare($sql);
-                
-                $consulta->execute();
-
-                return $consulta->fetchAll();
-
-            } catch(PDOException $e) {
-                echo $e;
-            }
-        }
-
-        // Deletar
-        public function deletar($id) {
-            $conexao = new Conexao();
-            $conn = $conexao->conectar();
-
-            try {
-<<<<<<< HEAD
-                $sql = "DELETE FROM produto WHERE id = :id;";
-=======
-                $sql = "DELETE FROM pessoas WHERE id = :id;";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-
-                $consulta = $conn->prepare($sql);
-
-                $consulta->bindValue(":id", $id);
-
-                $retorno = $consulta->execute();
-
-                if($retorno) {
-<<<<<<< HEAD
-                    echo "Produto deletado com sucesso";
-                } else {
-                    echo "Erro ao deletar produto";
-=======
-                    echo "Pessoa deletada com sucesso";
-                } else {
-                    echo "Erro ao deletar pessoa";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-                }
-            } catch(PDOException $e) {
-                echo $e;
-            }
-        }
-
-        // Editar
-<<<<<<< HEAD
-        public function editar($id,$titulo,$descricao,$quantidade,$preco) {
-=======
-        public function editar($id, $nome, $idade) {
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-            $conexao = new Conexao();
-            $conn = $conexao->conectar();
-
-            try {
-<<<<<<< HEAD
-                $sql = "UPDATE produto SET titulo = :titulo, descricao = :descricao, quantidade = :quantidade, preco = :preco WHERE id = :id";
-=======
-                $sql = "UPDATE pessoas SET nome = :nome, idade = :idade WHERE id = :id";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-
-                $consulta = $conn->prepare($sql);
-
-                $consulta->bindValue(":id", $id);
-<<<<<<< HEAD
-                $consulta->bindValue(":titulo", $titulo);
-                $consulta->bindValue(":descricao", $descricao);
-                $consulta->bindValue(":quantidade", $quantidade);
-                $consulta->bindValue(":preco", $preco);
-
-=======
-                $consulta->bindValue(":nome", $nome);
-                $consulta->bindValue(":idade", $idade);
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-
-                $retorno = $consulta->execute();
-
-                if($retorno) {
-<<<<<<< HEAD
-                    echo "Produto atualizada com sucesso";
-                } else {
-                    echo "Erro ao atualizar produto";
-=======
-                    echo "Pessoa atualizada com sucesso";
-                } else {
-                    echo "Erro ao atualizar pessoa";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-                }
-
-            } catch(PDOException $e) {
-                echo $e;
-            }
-        }
-
-        // Consultar
-        public function consultar($id) {
-            $conexao = new Conexao();
-            $conn = $conexao->conectar();
-
-            try {
-<<<<<<< HEAD
-                $sql = "SELECT * FROM produto WHERE id = :id;";
-=======
-                $sql = "SELECT * FROM pessoas WHERE id = :id;";
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-
-                $consulta = $conn->prepare($sql);
-                
-                $consulta->bindValue(":id", $id);
-
-                $consulta->execute();
-
-                $dados = $consulta->fetchAll()[0];
-                echo json_encode($dados);
-
-            } catch(PDOException $e) {
-                echo $e;
-            }
-<<<<<<< HEAD
-        }
-=======
-        }*/
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
     }
 
-?>
+    // Listar
+
+    public function listar()
+    {
+
+        $conexao = new Conexao();
+        $conn = $conexao->conectar();
+
+        try {
+
+            $sql = "SELECT * FROM produto;";
+
+
+            $consulta = $conn->prepare($sql);
+
+            $consulta->execute();
+
+            return $consulta->fetchAll();
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
+    // Deletar
+    public function deletar($id)
+    {
+        $conexao = new Conexao();
+        $conn = $conexao->conectar();
+
+        try {
+
+            $sql = "DELETE FROM produto WHERE id = :id;";
+
+
+
+            $consulta = $conn->prepare($sql);
+
+            $consulta->bindValue(":id", $id);
+
+            $retorno = $consulta->execute();
+
+            if ($retorno) {
+
+                echo "Produto deletado com sucesso";
+            } else {
+                echo "Erro ao deletar produto";
+            }
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
+    // Editar
+
+    public function editar($id, $titulo, $descricao, $quantidade, $preco)
+    {
+
+        $conexao = new Conexao();
+        $conn = $conexao->conectar();
+
+        try {
+
+            $sql = "UPDATE produto SET titulo = :titulo, descricao = :descricao, quantidade = :quantidade, preco = :preco WHERE id = :id";
+
+
+            $consulta = $conn->prepare($sql);
+
+            $consulta->bindValue(":id", $id);
+
+            $consulta->bindValue(":titulo", $titulo);
+            $consulta->bindValue(":descricao", $descricao);
+            $consulta->bindValue(":quantidade", $quantidade);
+            $consulta->bindValue(":preco", $preco);
+
+
+
+            $retorno = $consulta->execute();
+
+            if ($retorno) {
+
+                echo "Produto atualizada com sucesso";
+            } else {
+                echo "Erro ao atualizar produto";
+            }
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+
+    // Consultar
+    public function consultar($id)
+    {
+        $conexao = new Conexao();
+        $conn = $conexao->conectar();
+
+        try {
+            $sql = "SELECT * FROM produto WHERE id = :id;";
+
+
+            $consulta = $conn->prepare($sql);
+
+            $consulta->bindValue(":id", $id);
+
+            $consulta->execute();
+
+            $dados = $consulta->fetchAll()[0];
+            echo json_encode($dados);
+        } catch (PDOException $e) {
+            echo $e;
+        }
+    }
+}
--- backend/controllerProduto.php
+++ backend/controllerProduto.php
@@ -1,94 +1,54 @@
 <?php
 
-    require "Produto.class.php";
-    $produto = new Produto();
+require "Produto.class.php";
+$produto = new Produto();
 
-    if (isset($_POST['cadastrarProduto'])) {
-<<<<<<< HEAD
-        $titulo = (isset($_POST['titulo'])) ? $_POST['titulo'] : null;
-        $descricao = (isset($_POST['descricao'])) ? $_POST['descricao'] : null;
-        $quantidade = (isset($_POST['quantidade'])) ? $_POST['quantidade'] : null;
-        $preco = (isset($_POST['preco'])) ? $_POST['preco'] : null;
-        $produto->cadastrar($titulo,$descricao,$quantidade,$preco);
-    }
+if (isset($_POST['cadastrarProduto'])) {
 
-   if (isset($_GET['listarProduto'])) {
-        $dados = $produto->listar();
+    $titulo = (isset($_POST['titulo'])) ? $_POST['titulo'] : null;
+    $descricao = (isset($_POST['descricao'])) ? $_POST['descricao'] : null;
+    $quantidade = (isset($_POST['quantidade'])) ? $_POST['quantidade'] : null;
+    $preco = (isset($_POST['preco'])) ? $_POST['preco'] : null;
+    $produto->cadastrar($titulo, $descricao, $quantidade, $preco);
+}
 
-        foreach ($dados as $produto) {
-            echo "
+if (isset($_GET['listarProduto'])) {
+    $dados = $produto->listar();
+
+    foreach ($dados as $produto) {
+        echo "
                 <tr>
                     <td>" . $produto["titulo"] . "</td>
                     <td>" . $produto["descricao"] . "</td>
                     <td>" . $produto["quantidade"] . "</td>
                     <td>" . $produto["preco"] . "</td>
-                 <td onclick='deletarProduto(".$produto["id"].")'>Excluir</td>
-                    <td onclick='consultarProduto(".$produto["id"].")'>Editar</td>
-=======
-        $nome = (isset($_POST['titulo'])) ? $_POST['titulo'] : null;
-        $idade = (isset($_POST['descricao'])) ? $_POST['descricao'] : null;
-        $idade = (isset($_POST['quantidade'])) ? $_POST['quantidade'] : null;
+                    <td onclick='deletarProduto(" . $produto["id"] . ")'>Excluir</td>
+                    <td onclick='consultarProduto(" . $produto["id"] . ")'>Editar</td>
 
-        $pessoa->cadastrar($titulo, $descricao,$quantidade);
-    }
-
- /*   if (isset($_GET['listarPessoas'])) {
-        $dados = $pessoa->listar();
-
-        foreach ($dados as $pessoa) {
-            echo "
-                <tr>
-                    <td>" . $pessoa["nome"] . "</td>
-                    <td>" . $pessoa["idade"] . "</td>
-                    <td onclick='deletarPessoa(".$pessoa["id"].")'>Excluir</td>
-                    <td onclick='consultarPessoa(".$pessoa["id"].")'>Editar</td>
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
                 </tr>
             ";
-        }
     }
+}
 
-<<<<<<< HEAD
-    if (isset($_POST['deletarProduto'])) {
-        $id = (isset($_POST['id'])) ? $_POST['id'] : null;
 
-        $produto->deletar($id);
-    }
+if (isset($_POST['deletarProduto'])) {
+    $id = (isset($_POST['id'])) ? $_POST['id'] : null;
 
-    if (isset($_POST['editarProduto'])) {
-        $id = (isset($_POST['id'])) ? $_POST['id'] : null;
-        $titulo = (isset($_POST['titulo'])) ? $_POST['titulo'] : null;
-        $descricao = (isset($_POST['descricao'])) ? $_POST['descricao'] : null;
-        $quantidade = (isset($_POST['quantidade'])) ? $_POST['quantidade'] : null;
-        $preco = (isset($_POST['preco'])) ? $_POST['preco'] : null;
+    $produto->deletar($id);
+}
 
-        $produto->editar($id,$titulo,$descricao,$quantidade,$preco);
-    }
+if (isset($_POST['editarProduto'])) {
+    $id = (isset($_POST['id'])) ? $_POST['id'] : null;
+    $titulo = (isset($_POST['titulo'])) ? $_POST['titulo'] : null;
+    $descricao = (isset($_POST['descricao'])) ? $_POST['descricao'] : null;
+    $quantidade = (isset($_POST['quantidade'])) ? $_POST['quantidade'] : null;
+    $preco = (isset($_POST['preco'])) ? $_POST['preco'] : null;
 
-    if (isset($_GET['consultarProduto'])) {
-        $id = (isset($_GET['id'])) ? $_GET['id'] : null;
+    $produto->editar($id, $titulo, $descricao, $quantidade, $preco);
+}
 
-        $produto->consultar($id);
-    }
-=======
-    if (isset($_POST['deletarPessoa'])) {
-        $id = (isset($_POST['id'])) ? $_POST['id'] : null;
+if (isset($_GET['consultarProduto'])) {
+    $id = (isset($_GET['id'])) ? $_GET['id'] : null;
 
-        $pessoa->deletar($id);
-    }
-
-    if (isset($_POST['editarPessoa'])) {
-        $id = (isset($_POST['id'])) ? $_POST['id'] : null;
-        $nome = (isset($_POST['nome'])) ? $_POST['nome'] : null;
-        $idade = (isset($_POST['idade'])) ? $_POST['idade'] : null;
-
-        $pessoa->editar($id, $nome, $idade);
-    }
-
-    if (isset($_GET['consultarPessoa'])) {
-        $id = (isset($_GET['id'])) ? $_GET['id'] : null;
-
-        $pessoa->consultar($id);
-    }*/
->>>>>>> 9cbc7a8e73d36ea8122e4f6e878d7f0bb2767e16
-?>
+    $produto->consultar($id);
+}
